bd19b18cbbea86c9755b3e11d38b0f2db5cff054
Hakan-Soysal <66327932+Hakan-Soysal@users.noreply.github.com>
2023-01-16 16:35:50 +0300

    Initial Commit

---
+++ AMQP Deneme/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+// See https://aka.ms/new-console-template for more information
+using System.Net;
+using Apache.NMS.AMQP;
+
+Console.WriteLine("Hello, World!");
+
+
+
+var connectionFactory = new ConnectionFactory();
+connectionFactory.BrokerUri = new Uri("amqp://nftmarketplace.togg.cloud:8443");
+var connection = await connectionFactory.CreateConnectionAsync("", "");
+await connection.StartAsync();
+var x = connection.MetaData;
+
+Console.WriteLine("asdadasas");
+Console.Read();
